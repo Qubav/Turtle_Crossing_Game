2c286d43f4d293faabdd216fb54400881c2147fc
Qubav <124883831+Qubav@users.noreply.github.com>
2023-05-21 19:12:42 +0200

    adding comments and docstrings

--- scoreboard.py
+++ scoreboard.py
@@ -7,23 +7,31 @@ class Scoreboard(Turtle):
     
     def __init__(self) -> None:
         super().__init__()
+
+        # setting up variable value that stores level to 1
         self.level = 1
 
+        # executing starting methods
         self.penup()
         self.hideturtle()
         self.goto(SCOREBOARD_POSITION)
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        """Method updates scoreboard."""
+
         self.clear()
         self.write(f"Level: {self.level}", align = "left", font = FONT)
 
     def set_level_up(self):
+        """Method increases player score value by 1."""
 
         self.level += 1
     
     def game_over(self):
+        """Method writes \"GAME OVER\" in centre of the window and clears player score."""
+
         self.clear()
         self.goto(0, 0)
-        self.write(f"GAME OVEr", align = "center", font = FONT)
+        self.write(f"GAME OVER", align = "center", font = FONT)
         
