2b2fe784483b1f1ea66f5b3bb5659fcf85401d01
Qubav <124883831+Qubav@users.noreply.github.com>
2023-05-21 19:28:41 +0200

    adding docstrings and comments

--- player.py
+++ player.py
@@ -8,7 +8,9 @@ FINISH_LINE_Y = 280
 class Player(Turtle):
 
     def __init__(self) -> None:
-        super().__init__() 
+        super().__init__()
+
+        # executing methods
         self.penup()
         self.shape("turtle")
         self.color("black")
@@ -16,14 +18,19 @@ class Player(Turtle):
         self.goto(STARTING_POSITION)
     
     def move_forward(self):
+        """Method makes turtle go forward for fixed amount of units."""
 
         self.forward(MOVE_DISTANCE)
 
     def check_if_turtle_crossed_road(self):
+        """Method check if player has crossed road successfully, and returns True if player did or False if player didn't."""
 
-        if self.ycor() > 285:
+        if self.ycor() > FINISH_LINE_Y:
             return True
+        else:
+            return False
 
     def return_to_start(self):
+        """Method changes players position to starting position."""
 
         self.goto(STARTING_POSITION)
